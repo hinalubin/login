14977e787f9513d6bf7c275ee9e6dcfa7ae5bf36
hinalubin <hinalubin12@gmail.com>
2025-06-26 17:00:39 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+import 'package:login/welcome.dart';
+void main(){
+  runApp(MaterialApp(home: page1(),debugShowCheckedModeBanner: false,));
+}
---
+++ lib/welcome.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+class page1 extends StatelessWidget {
+  const page1({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 600,
+          decoration: BoxDecoration(
+            borderRadius: BorderRadiusGeometry.circular(10),
+            color: Colors.white,
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Text("Welcome", style: TextStyle(fontSize: 30)),
+              SizedBox(height: 100),
+              Image(
+                image: NetworkImage(
+                  "https://static.vecteezy.com/system/resources/thumbnails/011/432/528/small/enter-login-and-password-registration-page-on-screen-sign-in-to-your-account-creative-metaphor-login-page-mobile-app-with-user-page-flat-illustration-vector.jpg",
+                ),
+              ),
+              SizedBox(height: 30,),
+
+              SizedBox(
+                height: 50,
+                width: 330,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text('Login',style: TextStyle(color: Colors.black,fontSize: 15),),style: ElevatedButton.styleFrom(side: BorderSide(color: Colors.black,width: 1)),
+                ),
+              ),
+              SizedBox(height: 20),
+              SizedBox(
+                height: 50,
+                width: 330,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: const Text("Sign up",style: TextStyle(color: Colors.white,fontSize: 15),),
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
