741653e9f6a9e3571121165dca716d97517d03c5
hinalubin <hinalubin12@gmail.com>
2025-06-27 17:07:41 +0530

    signup done

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:login/signup.dart';
 import 'package:login/welcome.dart';
 void main(){
-  runApp(MaterialApp(home: page1(),debugShowCheckedModeBanner: false,));
+  runApp(MaterialApp(home: page2(),debugShowCheckedModeBanner: false,));
 }
---
+++ lib/signup.dart
@@ -0,0 +1,108 @@
+import 'package:flutter/material.dart';
+
+class page2 extends StatelessWidget {
+  const page2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.grey,
+      body: Center(
+        child: Container(
+          width: 350,
+          height: 600,
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadiusGeometry.circular(10),
+          ),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 20, right: 20),
+            child: Column(
+              children: [
+                SizedBox(height: 30),
+                Text("Sign up", style: TextStyle(fontSize: 30)),
+                SizedBox(height: 13),
+                Text(
+                  "Create an account,it's free",
+                  style: TextStyle(fontSize: 15, color: Colors.black),
+                ),
+                SizedBox(height: 30),
+                Align(child: Text("Username"), alignment: Alignment.topLeft),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Align(child: Text("Email"), alignment: Alignment.topLeft),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Align(child: Text("Password"), alignment: Alignment.topLeft),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                Align(
+                  child: Text("Confirm Password"),
+                  alignment: Alignment.topLeft,
+                ),
+                TextField(
+                  decoration: InputDecoration(
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(5),
+                      borderSide: BorderSide(color: Colors.grey),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 40),
+                SizedBox(
+                  height: 50,
+                  width: 330,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(color: Colors.white, fontSize: 15),
+                    ),
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.blue,
+                    ),
+                  ),
+                ),
+                SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text("Already have an account?"),
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.black),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
